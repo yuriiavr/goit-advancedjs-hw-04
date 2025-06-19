bebadee9cb777e1e8d9b1425fea23dae73cfab28
yuriiavr <yurii.avramets@gmail.com>
2025-06-19 18:19:31 +0300

    3312312

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-advancedjs-hw-03/',
+    base: '/goit-advancedjs-hw-04/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
